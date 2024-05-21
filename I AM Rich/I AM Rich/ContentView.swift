831566b24a8e66c0e41021ab706a9f62121be270
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2024-05-21 09:25:06 +0800

    Add files via upload

---
+++ I AM Rich/I AM Rich/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  I AM Rich
+//
+//  Created by Guo-Feng Luo on 2024/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I AM Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I AM Rich/I AM Rich/I_AM_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_AM_RichApp.swift
+//  I AM Rich
+//
+//  Created by Guo-Feng Luo on 2024/5/21.
+//
+
+import SwiftUI
+
+@main
+struct I_AM_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
